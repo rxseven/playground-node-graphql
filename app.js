cf0f7728f5432cc8829a4d9eb5acd2b316fc57cf
ME (Mac mini) <rxseven.com@gmail.com>
2019-01-03 18:32:04 +0700

    Add Express app & basic configuration

---
+++ app.js
@@ -0,0 +1,19 @@
+// Import modules
+const dotenv = require('dotenv');
+const express = require('express');
+const morgan = require('morgan');
+
+// Load environment variables
+dotenv.config({ path: '.env' });
+
+// Create Express app
+const app = express();
+
+// Configure app
+app.set('port', process.env.PORT);
+
+// Configure middleware
+app.use(morgan('dev'));
+
+// Export modules
+module.exports = app;

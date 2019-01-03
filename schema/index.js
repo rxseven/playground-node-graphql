a803b0bf3862f09219222768e38d8cdffa69843e
ME (Mac mini) <rxseven.com@gmail.com>
2019-01-03 18:47:17 +0700

    Add Company type

--- schema/index.js
+++ schema/index.js
@@ -7,6 +7,16 @@ const { GraphQLInt, GraphQLObjectType, GraphQLString } = graphql;
 // Constants
 const BASE_URL = 'http://localhost:3000';
 
+// Company type
+const CompanyType = new GraphQLObjectType({
+  fields: () => ({
+    description: { type: GraphQLString },
+    id: { type: GraphQLString },
+    name: { type: GraphQLString }
+  }),
+  name: 'Company'
+});
+
 // User type
 const UserType = new GraphQLObjectType({
   fields: () => ({
